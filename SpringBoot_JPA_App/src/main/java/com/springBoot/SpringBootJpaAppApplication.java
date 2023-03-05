74400ed84395153322ff1e16abd69cef2adb71f0
naman kumar singh <singhnaman320@gmail.com>
2023-03-05 20:18:45 +0530

    pom and main class updation

--- SpringBoot_JPA_App/src/main/java/com/springBoot/SpringBootJpaAppApplication.java
+++ SpringBoot_JPA_App/src/main/java/com/springBoot/SpringBootJpaAppApplication.java
@@ -3,6 +3,19 @@ package com.springBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+// For swagger as it is working for cloud not on local host and also we are 
+// using https not http 
+
+@OpenAPIDefinition(
+		servers= {
+				
+				@Server(url = "/", description= "Default Server URL")
+		}
+	)
+
 @SpringBootApplication
 public class SpringBootJpaAppApplication {
 
